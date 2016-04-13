a0f84298f145fe0d5da4e26fc54de435b530f105
mohamedelfiky <mohamed.elfikyy@yahoo.com>
2016-04-13 13:01:54 +0200

    add rails api and create db

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Pinews::Application.config.secret_token = '11009abca86070f09642b0af08e764b1159d1934da6cca836decb7a28ac084364d523fea7ff807cbfa7bfa2d93b1be49374ddb12674a138a9eb413eae0fc880d'
