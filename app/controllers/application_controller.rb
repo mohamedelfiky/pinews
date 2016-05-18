360b2386a7ef633957669b056e6bfc52d3a3b162
mohamedelfiky <mohamed.elfikyy@yahoo.com>
2016-05-18 02:20:40 +0200

    minor cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,25 +4,26 @@ class ApplicationController < ActionController::API
   include ErrorHandlers
   include AuthProtected
 
-  before_filter :configure_devise_params, if: :devise_controller?
+  before_action :configure_devise_params, if: :devise_controller?
 
   def configure_devise_params
-    devise_parameter_sanitizer.for(:sign_up) << [:name, :nickname]
+    devise_parameter_sanitizer.for(:sign_up) << %i(name nickname)
   end
 
   protected
+
   def self.set_pagination_headers(name, options = {})
-    after_filter(options) do |controller|
-      results = instance_variable_get("@#{name}")
+    after_action(options) do
+      results = instance_variable_get("@#{ name }")
       headers['X-Pagination'] = {
-          total: results.total_entries,
-          totalPages: results.total_pages,
-          firstPage: results.current_page == 1,
-          lastPage: results.next_page.blank?,
-          previousPage: results.previous_page,
-          nextPage: results.next_page,
-          outOfBounds: results.out_of_bounds?,
-          offset: results.offset
+        total: results.total_entries,
+        totalPages: results.total_pages,
+        firstPage: results.current_page == 1,
+        lastPage: results.next_page.blank?,
+        previousPage: results.previous_page,
+        nextPage: results.next_page,
+        outOfBounds: results.out_of_bounds?,
+        offset: results.offset
       }.to_json
     end
   end
--- app/controllers/concerns/auth_protected.rb
+++ app/controllers/concerns/auth_protected.rb
@@ -2,28 +2,26 @@ module AuthProtected
   extend ActiveSupport::Concern
 
   included do
-    before_action :authenticate_current_user, except: [:index, :show, :count], unless: :devise_controller?
+    before_action :authenticate_current_user, except: %i(index show count), unless: :devise_controller?
   end
-
 
   def authenticate_current_user
-    head :unauthorized if get_current_user.nil?
+    head :unauthorized if detect_current_user.nil?
   end
 
-  def get_current_user
+  def detect_current_user
     auth_headers = request.cookies['auth_headers'] || request.headers['Authorization']
     return nil unless auth_headers
 
     auth_headers = JSON.parse(auth_headers)
-    expiry, uid, client= auth_headers['expiry'], auth_headers['uid'], auth_headers['client']
+    expiry, uid, client = auth_headers['expiry'], auth_headers['uid'], auth_headers['client']
 
     expiration_datetime = DateTime.strptime(expiry, '%s')
     current_user = User.find_by(uid: uid)
 
-    if current_user && current_user.tokens.has_key?(client) && expiration_datetime > DateTime.now
+    if current_user && current_user.tokens.key?(client) && expiration_datetime > DateTime.current
       @current_user = current_user
     end
     @current_user
   end
-
 end
